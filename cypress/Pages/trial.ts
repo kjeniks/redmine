89c55256d9228fa1c1ebb187e394dc6f395f5af4
kjeniks <kjeniks@gmail.com>
2021-11-25 13:26:46 +0100

    full-untested

--- cypress/Pages/trial.ts
+++ cypress/Pages/trial.ts
@@ -1,20 +1,31 @@
 export default class Trial {
         
         el = {
-                hottext: ".tag-hot-text",
-                h1: "h1.heading-2",
-                h2: "h2.heading-4",
-                h3: "h3.heading-5",
-                h410: "h4.heading-10",
-                h4: "h4.heading-4",
-                footer: ".footer-wrap",
-                footerContext: ".footer-flex-container",
-                logoFooter: "a.footer-logo-link",
-                logoImg: "img.logo-image-2",
-                navigation: ".navigation-items.sc-top-nav",
-                logoHeader: "a.logo-link",
-                menu: ".navigation-item-2",
-                language: ".wg-element-wrapper.sw2"
+
+                formNameLabel:'label[for="user_firstname"]',
+                formNameInput:'input#user_firstname',
+                formLastNameLabel:'label[for="user_lastname"]',
+                formLastNameInput:'input#user_lastname',
+                formPhoneLabel:'label[for="user_marketing_phone"]',
+                formPhoneInput:'input#marketing_phone',
+                formCompanyLabel:'label[for="user_company"]',
+                formCompanyInput:'input#user_company',
+                formLanguageLabel:'label[for="user_language"]',
+                formLanguageSelect:'select#user_language',
+                formRoleLabel:'label[for="user_easy_user_type_id"]',
+                formRoleSelect:'select#user_easy_user_type_id',
+                formCheckBoxGDPR:'#user_gdpr_consent_given',
+                formSubmitButton:'button#submit_onboard_button_modal',
+                avatarProfile: '#top-menu > #loggedas > span.avatar-container > a > img.gravatar.small',
+                editProfileModal:'.ui-dialog.ui-corner-all.ui-widget.ui-widget-content.ui-front.modal.ui-draggable',
+                editProfileButton: '.button-positive.icon.icon-edit',
+                editPassword:'input#user_password',
+                editPasswordConfirm:'input#user_password_confirmation',
+                saveEditButton: 'input.button-positive',
+
+                login: 'input#username',
+                logPassword: 'input#password',
+                loginButton: 'button.f-form__btn-login'
     
         }
     
@@ -39,6 +50,65 @@ export default class Trial {
                         .clear().type(mail)
                 cy.get('button#mauticform_input_trialrequest_submit').click()
                 cy.wait(10000)
+        }
+
+        fillFirstLoginForm(person){
+                cy.contains('Welcome! Finish the setup of your account.').should('be.visible')
+                
+                cy.get('#invited_colleague_onboard_modal').should('be.visible').within(()=>{
+                        //General check of elements on modal 
+                        cy.get(this.el.formNameLabel).contains('First name').should('be.visible')
+                        cy.get(this.el.formNameInput).should('be.visible')
+        
+                        cy.get(this.el.formLastNameLabel).contains('Last name').should('be.visible')
+                        cy.get(this.el.formLastNameInput).should('be.visible')
+        
+                        cy.get(this.el.formPhoneLabel).contains('Phone').should('be.visible')
+                        cy.get(this.el.formPhoneInput).should('be.visible')
+        
+                        cy.get(this.el.formCompanyLabel).contains('Company').should('be.visible')
+                        cy.get(this.el.formCompanyInput).should('be.visible')
+        
+                        cy.get(this.el.formLanguageLabel).contains('Language').should('be.visible')
+                        cy.get(this.el.formLanguageSelect).should('be.visible')
+        
+                        cy.get(this.el.formRoleLabel).contains('My role').should('be.visible')
+                        cy.get(this.el.formRoleSelect).should('be.visible')
+        
+                        cy.get(this.el.formCheckBoxGDPR).should('be.visible')
+                        cy.get('.easy-invite-colleagues__gdpr-link').should('be.visible')
+                        
+        
+                        cy.get(this.el.formNameInput).clear().type(person.name,{ delay: 50 })
+                        cy.get(this.el.formLastNameInput).clear().type(person.surname,{ delay: 50 })
+                        cy.get(this.el.formPhoneInput).clear().type(person.phone,{ delay: 50 })
+                        cy.get(this.el.formLanguageSelect).select(person.language).wait(50)
+                        cy.get(this.el.formRoleSelect).select(person.role,{force:true}).wait(50)
+                        cy.get(this.el.formCheckBoxGDPR).check().wait(50)
+                })
+                cy.get(this.el.formSubmitButton).contains('Submit').should('be.visible')
+                cy.get(this.el.formSubmitButton).click()
+
+                //cy.contains('Thank you, your application is being set up. This might take few seconds.').should('be.visible')
+                cy.get('.flash.notice',{timeout:60000}).contains('User data updated').should('be.visible',{timeout:60000})
+        }
+        editUserProfile(person){
+                cy.get(this.el.avatarProfile).click()
+                cy.get(this.el.editProfileModal).should('be.visible',{timeout:20000})
+                cy.get(this.el.editProfileButton).should('be.visible').click()
+                cy.get('.easy-content-page').should('be.visible').within(()=>{
+                        cy.get('h2').contains(person.mail).should('be.visible')
+                        cy.get(this.el.editPassword).type(person.password,{ delay: 50 })
+                        cy.get(this.el.editPasswordConfirm).type(person.password,{ delay: 50 })
+                        cy.get(this.el.saveEditButton).should('have.value','Save').click()
+                })
+                cy.get('.flash.error').contains('Your session has expired. Please login again.').should('be.visible')
+        }
+        loginwithNewPassword(person){
+                cy.get(this.el.login).should('be.visible').type(person.mail,{ delay: 50 })
+                cy.get(this.el.logPassword).should('be.visible').type(person.password,{ delay: 50 })
+                cy.get(this.el.loginButton).click()
+                cy.get('h2').contains(person.mail).should('be.visible')
         }
       
     }
--- cypress/integration/createLead.spec.ts
+++ cypress/integration/createLead.spec.ts
@@ -1,18 +1,29 @@
 import urls from "@constants/urls"
 
 import Trial from "@pages/trial"
+import { random } from "cypress/types/lodash"
 import faker from "faker"
 
 const trial = new Trial()
 
+const role = [
+    'Project Manager',
+    'Portfolio Manager',
+    'Team Leader',
+    'CEO',
+    'Client',
+    'External'
+
+]
+
 const person = {
     name: 'QA',
-    surname: 'Appliucant',
+    surname: 'Applicant',
     phone: '+420222980481',
     language: 'English',
     mail: 'qa-applicant@easy.cz',
-    password: 'qa-test'
-    
+    password: 'qa-test',
+    role: role[faker.random.number(5)]
 }
 
 describe('Start a trieal', () => {
@@ -24,7 +35,10 @@ describe('Start a trieal', () => {
         trial.submitByMail(person.mail)
         //cy.contains('Your trial will be ready soon and you will be redirected into it.',{timeout:60000}).should('be.visible')
         cy.intercept('GET','assets/first_login_form*').as('firstLoginForm')
-        cy.wait('@firstLoginForm',{timeout:500000})
+        cy.wait('@firstLoginForm',{timeout:120000})
+        trial.fillFirstLoginForm(person)
+        trial.editUserProfile(person)
+        trial.loginwithNewPassword(person)
         
     })    
 })    
